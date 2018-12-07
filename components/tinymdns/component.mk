260fc58a51efb52ecaab9795f8348e6254a4e9e7
ouyang <ouyangchengfeng@xiaomi.com>
2018-12-07 13:56:11 +0800

    update tinymdns

--- components/tinymdns/component.mk
+++ components/tinymdns/component.mk
@@ -1,11 +1,14 @@
 #
 # Component Makefile
 #
-COMPONENT_ADD_INCLUDEDIRS += tinymdns/source/src/TinyMDNS
+COMPONENT_ADD_INCLUDEDIRS += tinymdns/source/src/dns-common \
+                             tinymdns/source/src/multicast-dns \
+                             tinymdns/source/src/dns-server-captive-portal
 
-COMPONENT_SRCDIRS += tinymdns/source/src/TinyMDNS \
-		     tinymdns/source/src/TinyMDNS/codec \
-		     tinymdns/source/src/TinyMDNS/message
+COMPONENT_SRCDIRS += tinymdns/source/src/multicast-dns \
+		     tinymdns/source/src/dns-common/codec \
+		     tinymdns/source/src/dns-common/message \
+		     tinymdns/source/src/dns-server-captive-portal
 
 CFLAGS += -DTINY_STATIC \
           -DESP8266 \
